1d39cfff0a0c9a06de1ad44d678645e208cc9fae
Jonathan Callahan <jtc7537@gmail.com>
2024-07-12 12:20:43 -0500

    cleanup

--- handler.py
+++ handler.py
@@ -1,31 +1,28 @@
-from typing import Dict, List, Any
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from accelerate import Accelerator
+from safetensors.torch import load_file
+import os
 
-class EndpointHandler():
+class EndpointHandler:
     def __init__(self, path=""):
-        # load the model for text generation
+
+        accelerator = Accelerator()
+
         model = AutoModelForCausalLM.from_pretrained(path)
         tokenizer = AutoTokenizer.from_pretrained(path)
-        # create inference pipeline
+
+        state_dict = load_file(os.path.join(path, "adapter_model.safetensors"))
+        model.load_state_dict(state_dict, strict=False)
+
+        model = accelerator.prepare(model)
+
         self.pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-    def __call__(self, data: Any) -> List[Dict[str, Any]]:
-        """
-        Args:
-            data (:obj:`dict`):
-                includes the input data and the parameters for the inference.
-        Return:
-            A :obj:`list`:. The object returned should be a list like [{"generated_text": "The generated text"}] containing:
-                - "generated_text": A string representing the generated text.
-        """
+    def __call__(self, data):
         inputs = data.pop("inputs", data)
         parameters = data.pop("parameters", None)
-
-        # pass inputs with all kwargs in data
         if parameters is not None:
             prediction = self.pipeline(inputs, **parameters)
         else:
             prediction = self.pipeline(inputs)
-        
-        # postprocess the prediction
         return prediction
