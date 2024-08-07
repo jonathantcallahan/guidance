9492fd201c46e52aa148368fc77d2b421e2ce55e
Jonathan Callahan <jtc7537@gmail.com>
2024-08-07 17:26:09 -0500

    RAG

--- guidanceapp/src/routes/text-generation/+server.js
+++ guidanceapp/src/routes/text-generation/+server.js
@@ -1,4 +1,3 @@
-//import { HUGGINGFACE_API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 import weaviate from 'weaviate-client';
 
@@ -21,19 +20,22 @@ async function nearTextQuery(question) {
 	});
 	return result;
 }
-// @ts-ignore
+
 export async function POST( {request} ) {
 	
 	const requestText = await request.json()
-	console.log(requestText)
 
 	const vectorResult = await nearTextQuery(requestText.question);
-	const vectorResultClean = vectorResult.objects[0].properties.question;
-	console.log(vectorResultClean);
+	console.log("vector result\n" + JSON.stringify(vectorResult));
+	const vectorResultClean = vectorResult.objects[0].properties.answer;
+	const distance = vectorResult.objects[0].metadata?.distance || 1;
+	console.log("distance: " + distance)
+
+	let inputRAG = `Below is an instruction that describes a task, paired with an input that provides further context. Using the provided materials, return a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. As always, do not diverge from your standard speech patterns and do not over-embellish. Please answer the following question using the content from the reference text that follows it. You may change the content so that it more directly addresses the question being posed in a semantically correct manner. Do not repeat the question, simply answer the question using the concepts in the refernce text. \n### Input:\n${ requestText.question } <start of reference text> ${ vectorResultClean } <end of the reference text> \n### Response:\n`;
+	let inputsNoRAG = `Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. Please answer the following question using your standard speech patterns but do not over-embellish.\n### Input:\n${ requestText.question }\n### Response:\n`
 	
 	const requestBody = { 
-		inputs: `Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. Please answer the following question using the content from the text block that follows it. Transform the text only enought that it is an acceptable answer to the question. As always, do not diverge from your standard speech patterns and do not over-embellish.\n### Input:\n${ requestText.question } end of question, start of reference text: ${ vectorResultClean } \n### Response:\n`,
-		//inputs: `Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. Please answer the following question using your standard speech patterns but do not over-embellish.\n### Input:\n${ requestText.question }\n### Response:\n`,
+		inputs: distance > .2 ? inputsNoRAG : inputRAG,
 		parameters: {}
 	}
 	try {
@@ -51,7 +53,7 @@ export async function POST( {request} ) {
 		);
 
 		const result = await response.json()
-		console.log(result)
+		console.log("hf response\n" + JSON.stringify(result))
 		const processedResult = result[0].generated_text.split('### Response:')[1]
 		return json({ contents: processedResult });
 
