c656923f83a49d0403c1ca8fdec4c3db97f44e47
Jonathan Callahan <jtc7537@gmail.com>
2024-08-10 21:37:56 -0500

    front-end

--- guidanceapp/src/routes/+page.svelte
+++ guidanceapp/src/routes/+page.svelte
@@ -6,12 +6,13 @@
     
     interface Component {
         id: Number,
-        type: 'command' | 'output'
+        type: 'command' | 'output',
+        prompt?: CustomEvent
     }
 
     let components = writable<Component[]>([]);
 
-    function addCommand(prefill) {
+    function addCommand(prefill?: CustomEvent) {
         components.update(n => [
             ...n,
             { id: n.length + 1, type: 'command', prefill: prefill}
@@ -28,7 +29,7 @@
     let ref: HTMLElement;
 
     onMount(() => {
-        addCommand('');
+        addCommand();
     })
 
     function handleFocus() {
@@ -49,52 +50,14 @@
     let question = '';
     let processedResponse = ''
 
-    async function getVectorSearch() {
-        console.log(question)
-        let response = await fetch('/vector-database', {
-            method: 'POST',
-            body: JSON.stringify({ question }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        processedResponse = await response.json()
-    }
-
-    async function getAnswer() {
-        let response = await fetch('/text-generation', {
-			method: 'POST',
-			body: JSON.stringify({ question }),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-
-        const responseData = await response.json();
-        processedResponse = responseData.contents 
-    };
-
-    // function handleCommand(textContent) {
-    //     console.log(textContent.detail.textContent);
-    //     const newOutput = new CommandOutput({
-    //         target: document.querySelector('#command-line')
-    //     })
-    // }
 </script>
-
-<input bind:value={question}/>
-<button on:click={getAnswer}>get answer</button>
-<button on:click={getVectorSearch}>Search text for related snippet</button>
- 
-<p>{processedResponse}</p>
 
 <div id='command-line'>
     {#each $components as component (component.id )}
         {#if component.type == 'command'}
             <CommandBlock bind:ref on:mount={handleFocus} on:command={addResponse} id={component.id} />
         {:else}
-            <CommandOutput prompt={component.prompt} on:mount={addCommand} id={component.id} />
+            <CommandOutput prompt={component.prompt} on:output={addCommand} id={component.id} />
         {/if}
     {/each}
-    <!-- <CommandBlock bind:ref on:command={handleCommand}/> -->
 </div>
--- guidanceapp/src/routes/command-output.svelte
+++ guidanceapp/src/routes/command-output.svelte
@@ -4,30 +4,95 @@
 
     export let id;
     export let prompt;
-    let promptDetails = {
-        commands: {
+
+    let initQuery = false
+    let initQueryLoading = false
+    let question = ''
+    let processedResponse = ''
+    const loadingChars = ['⠁','⠂','⠄','⡀','⢀','⠠','⠐','⠈']
+    let activeCharOffset = 0
+    setInterval(() => activeCharOffset = activeCharOffset > 6 ? 0 : activeCharOffset + 1, 75)
+
+    let pD = {
+        com: {
             ask: / ask( |$)/.test(prompt.detail.textContent),
             welcome: / welcome( |$)/.test(prompt.detail.textContent),
             about: / about( |$)/.test(prompt.detail.textContent),
-            audio: / audio( |$)/.test(prompt.detail.textContent)
+            audio: / audio( |$)/.test(prompt.detail.textContent),
+            alan: /( |^)alanbotts /.test(prompt.detail.textContent)
         },
-        options: {
+        opt: {
             help: / -h( |$)| --help( |$)/.test(prompt.detail.textContent),
             version: / -v( |$)| --version( |$)/.test(prompt.detail.textContent),
             plain: / -p( |$)| --plain( |$)/.test(prompt.detail.textContent),
             generate: / -g( |$)| --generate( |$)/.test(prompt.detail.textContent),
             library: / -l( |$)| --library( |$)/.test(prompt.detail.textContent)
         },
-        promptText: prompt.detail.textContent.match(/(?<=").*(?=")/)
+        text: prompt.detail.textContent.match(/(?<=").*(?=")/)
     };
+
+    async function getVectorSearch() {
+        console.log(question)
+        let response = await fetch('/vector-database', {
+            method: 'POST',
+            body: JSON.stringify({ question }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        processedResponse = await response.json()
+    }
+
+    async function getAnswer() {
+
+        let response = await fetch('/text-generation', {
+			method: 'POST',
+			body: JSON.stringify({ question: pD.text }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+        const responseData = await response.json();
+        processedResponse = responseData.contents 
+    };
+
+    function commandLogic() {
+        if (!pD.com.alan) {
+            processedResponse = 'Command not recognized. Try starting your submission with the string alanbotts'
+        } else {
+            initQuery = true
+            initQueryLoading = true
+            getAnswer()
+            $: initQueryLoading = processedResponse ? false : true 
+        }
+
+
+        dispatch('output', {})
+    }
     
     const dispatch = createEventDispatcher();
     onMount(() => {
-        let rawPrompt = prompt.detail.textContent
-        dispatch('mount', {})
-        console.log(JSON.stringify(promptDetails))
+        //let rawPrompt = prompt.detail.textContent
+        console.log(JSON.stringify(pD))
+        commandLogic()
     })
 </script>
+
+<style>
+    .markup-command {
+        color: red
+    }
+</style>
+
 <div id={id}>
-    hello<br><br>
+    {@html processedResponse}<br><br>
+    {#if initQuery}
+    <div>
+        <span>{initQueryLoading ? loadingChars[0 + activeCharOffset > 6 ? (0 + activeCharOffset - 7) : 0 + activeCharOffset] : 3}</span>
+        <span>{initQueryLoading ? loadingChars[1 + activeCharOffset > 6 ? (1 + activeCharOffset - 7) : 1 + activeCharOffset] : 3} </span>
+        <span>{initQueryLoading ? loadingChars[2 + activeCharOffset > 6 ? (2 + activeCharOffset - 7) : 2 + activeCharOffset] : 3}</span>
+        <span>Initiating query</span>
+    </div>
+    {/if}
 </div>
