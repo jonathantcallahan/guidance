782dca2338f861070c23f2fffcee24f3010288c5
Jonathan Callahan <jtc7537@gmail.com>
2024-08-17 13:36:19 -0500

    api

--- guidanceapp/src/routes/text-generation/+server.js
+++ guidanceapp/src/routes/text-generation/+server.js
@@ -1,5 +1,6 @@
-import { json } from '@sveltejs/kit';
-import weaviate from 'weaviate-client';
+import { json } from '@sveltejs/kit'
+import weaviate from 'weaviate-client'
+import { HfInference } from '@huggingface/inference'
 
 const client = await weaviate.connectToWeaviateCloud(
 	process.env.CLUSTER_URL || 'null',
@@ -21,49 +22,62 @@ async function nearTextQuery(question) {
 	return result;
 }
 
+
 export async function POST( {request} ) {
 	
 	const requestText = await request.json()
 	console.log('request received')
 
-	// const vectorResult = await nearTextQuery(requestText.question);
-	// console.log("vector result\n" + JSON.stringify(vectorResult));
+	const vectorResult = await nearTextQuery(requestText.question);
+	console.log("vector result\n" + JSON.stringify(vectorResult));
 
-	// const vectorResultClean = vectorResult.objects[0].properties.answer;
-	// const distance = vectorResult.objects[0].metadata?.distance || 1;
+	const vectorResultClean = vectorResult.objects[0].properties.answer;
+	const distance = vectorResult.objects[0].metadata?.distance || 1;
 
-	// console.log(vectorResultClean)
+	console.log(vectorResultClean)
 
-	// let inputRAG = `Below is an instruction that describes a task, paired with an input that provides further context. Using the provided materials, return a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. As always, do not diverge from your standard speech patterns and do not over-embellish. Please answer the following question using the content from the reference text that follows it. You may change the content so that it more directly addresses the question being posed in a semantically correct manner. Do not repeat the question, simply answer the question using the concepts in the refernce text. \n### Input:\n${ requestText.question } <start of reference text> ${ vectorResultClean } <end of the reference text> \n### Response:\n`;
+	let inputRAG = `Below is an instruction that describes a task, paired with an input that provides further context. Using the provided materials, return a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. As always, do not diverge from your standard speech patterns and do not over-embellish. Please answer the following question using the content from the reference text that follows it. You may change the content so that it more directly addresses the question being posed in a semantically correct manner. Do not repeat the question, simply answer the question using the concepts in the refernce text. \n### Input:\n${ requestText.question } <start of reference text> ${ vectorResultClean } <end of the reference text> \n### Response:\n`;
 	let inputsNoRAG = `Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n### Instruction: \nYou are English author and intellectual Alan Watts. Please answer the following question using your standard speech patterns but do not over-embellish.\n### Input:\n${ requestText.question }\n### Response:\n`
 	
+
+
 	const requestBody = { 
-		inputs: inputsNoRAG,// distance > .2 ? inputsNoRAG : inputRAG,
+		inputs: distance > .2 ? inputsNoRAG : inputRAG,
 		parameters: {}
 	}
 	try {
-		const response = await fetch(
-			"https://jq3a4fn9siusw6ee.us-east-1.aws.endpoints.huggingface.cloud",
-			{
-				headers: { 
-					"Accept" : "application/json",
-					"Authorization": `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
-					"Content-Type": "application/json" 
-				},
-				method: "POST",
-				body: JSON.stringify(requestBody),
-				signal: AbortSignal.timeout(30000)
-			}
-		);
+		// console.log(requestBody)
+		// const response = await fetch(
+		// 	"https://jq3a4fn9siusw6ee.us-east-1.aws.endpoints.huggingface.cloud",
+		// 	{
+		// 		headers: { 
+		// 			"Accept" : "application/json",
+		// 			"Authorization": `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+		// 			"Content-Type": "application/json" 
+		// 		},
+		// 		method: "POST",
+		// 		body: JSON.stringify(requestBody),
+		// 		signal: AbortSignal.timeout(45000)
+		// 	}
+		// );
 
-		const result = await response.json()
-		console.log("hf response\n" + JSON.stringify(result))
-		const processedResult = result[0]?.generated_text?.split('### Response:')[1]
-		return json({ contents: processedResult });
+		// const result = await response.json()
+		
+		
+		const inference = new HfInference(process.env.HUGGING_FACE_API_KEY)
+		const llama = inference.endpoint('https://jq3a4fn9siusw6ee.us-east-1.aws.endpoints.huggingface.cloud')
+		const { generated_text } = await llama.textGeneration(requestBody)
+
+		//console.log("hf response\n" + JSON.stringify(result))
+		console.log(generated_text)
+		let processedText = generated_text?.split('### Response:')[1]
+		//const processedResult = result[0]?.generated_text?.split('### Response:')[1]
+		//return json({ contents: processedResult });
+		return json({contents: processedText})
 
 	} catch (err) {
 		console.log(err)
-		return json({ contents: 'Endpoint initializing... Try again in 15 minutes'})
+		return json({ contents: 'Error on server'})
 	}
 	
 }
